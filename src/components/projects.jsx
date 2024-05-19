3ae9c7da9c08e58fe6a255c4aa3011b34a0cd339
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-05-19 10:48:13 -0500

    portfolio is working with map of projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,31 +1,42 @@
+import fillmybelly from "../assets/fillmybelly.png";
+import notetaker from "../assets/notetaker.png";
+import weatheroutlook from "../assets/weatheroutlook.png";
+import technicallyspeaking from "../assets/technicallyspeaking.png";
+import tracknheal from "../assets/tracknheal.png";
+
 const projects = [
   {
-    name: "note-taker",
-    img: "../assets/notetaker.png",
+    id: 1,
+    name: "Note Taker",
+    img: notetaker,
     url: "https://agile-caverns-74604-c775517e8f5a.herokuapp.com/",
     github: "https://github.com/courtneyhook/note-taker",
   },
   {
-    name: "weather-outlook",
-    img: "../assets/weatheroutlook.png",
+    id: 2,
+    name: "Weather Dashboard",
+    img: weatheroutlook,
     url: "https://courtneyhook.github.io/weather-dashboard/",
     github: "https://github.com/courtneyhook/weather-dashboard",
   },
   {
-    name: "technically-speaking",
-    img: "../assets/technicallyspeaking.png",
+    id: 3,
+    name: "Technically Speaking",
+    img: technicallyspeaking,
     url: "https://evening-waters-35264-00d72de9aee1.herokuapp.com/",
     github: "https://github.com/courtneyhook/technically-speaking-blog",
   },
   {
-    name: "fill-my-belly",
-    img: "../assets/fillmybelly.png",
+    id: 4,
+    name: "Fill My Belly",
+    img: fillmybelly,
     url: "https://jacksonjjones.github.io/Fill_My_Belly/",
     github: "https://github.com/jacksonjjones/Fill_My_Belly",
   },
   {
-    name: "track-n-heal",
-    img: "../assets/tracknheal.png",
+    id: 5,
+    name: "TrackNHeal",
+    img: tracknheal,
     url: "https://tracknheal-be5b35216eb1.herokuapp.com/",
     github: "https://github.com/prismhead26/Patient-Portal-App-Team-Colab",
   },
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+import projects from "../components/projects";
+
 export default function Portfolio() {
-  return <h1>Portfolio</h1>;
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={projects.id}>
+            <img
+              src={project.img}
+              alt={`screenshot of ${project.name} website`}
+              width="300"
+            />
+            <br />
+            {`${project.name}`}
+            <br />
+            <Link to={project.github}>Github Repository</Link>
+            <br />
+            <Link to={project.url}>Visit this site!</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
