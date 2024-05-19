d1c1c44b5fbc0077ddb22d7bb3346500e5d05aa6
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-05-19 02:08:21 -0500

    about me page is complete

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,5 +1,27 @@
 function AboutMe() {
-  return <h1>About Me</h1>;
+  return (
+    <>
+      <h1>About Me</h1>
+      <p>
+        My name is Courtney Hook. I am a third grade teacher at Mountain Grove
+        Elementary School. I have been teaching for 15 years. I am married with
+        two children. I have 4 cats. I love to cook, knit and work on creative
+        projects. I am an avid fan of Dateline and true crime mysteries.
+      </p>
+      <p>
+        Early in my teaching career, I realized that teachers need quality
+        resources for their students. I started dabbling in HTML about ten years
+        ago and decided to make a website two years ago. It made me feel
+        accomplished when I published my first site, but I knew there was much
+        more to learn. That is when I decided to complete the coding bootcamp.
+      </p>
+      <p>
+        My goals are to create resources for teachers and students to enhance
+        the school experience. I have been a dedicated professional for many
+        years and I have seen the needs in classrooms and home learning.
+      </p>
+    </>
+  );
 }
 
 export default AboutMe;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,13 @@ function Contact() {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    setuserName(" ");
+    setEmail(" ");
+    setMessage(" ");
     console.log("submitted");
+    console.log(userName);
+    console.log(email);
+    console.log(message);
   };
 
   return (
@@ -15,17 +21,26 @@ function Contact() {
       <h3>Contact Me</h3>
       <form onSubmit={handleFormSubmit}>
         <label htmlFor="userName">Name:</label>
-        <input value={userName} name="userName" type="text" />
+        <input
+          name="userName"
+          type="text"
+          onChange={(e) => setuserName(e.target.value)}
+        />
         <label htmlFor="email">Email:</label>
-        <input value={email} name="email" type="text" />
+        <input
+          name="email"
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="message">Message:</label>
         <textarea
-          value={message}
           name="message"
           type="text"
           rows={10}
           cols={40}
+          onChange={(e) => setMessage(e.target.value)}
         />
+        <button type="submit">Submit</button>
       </form>
     </>
   );
