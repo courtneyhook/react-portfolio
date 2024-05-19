1706685fbf17c7a371470b8a7bec7a96e50ccbf3
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-05-19 02:16:04 -0500

    resume complete

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,3 +1,29 @@
 export default function Resume() {
-  return <h1>Resume</h1>;
+  return (
+    <>
+      <h1>Resume</h1>
+      <h3>Front-End Proficiencies</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+        <li>Responsive Design</li>
+      </ul>
+      <h3>Back-End Proficiencies</h3>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL</li>
+        <li>Sequelize</li>
+        <li>MongoDB</li>
+        <li>REST</li>
+        <li>MERN</li>
+        <li>GraphQL</li>
+      </ul>
+    </>
+  );
 }
